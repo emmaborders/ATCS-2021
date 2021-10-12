282aa3c8e778becf9af2eb5615aa963a86f67eac
emma bordres <emma.borders@menloschool.org>
2021-10-12 11:49:03 -0700

    Variables, Strings, Numbers Exercises

---
+++ project0/firstName.py
@@ -0,0 +1,5 @@
+firstName = "emma"
+
+print(firstName)
+print(firstName.title())
+print(firstName.upper())
---
+++ project0/order.py
@@ -0,0 +1,5 @@
+standard = 3*2**3/6
+nonstandard = (3*2)**3/6
+
+print(standard)
+print(nonstandard)
