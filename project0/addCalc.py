cffabae0712db38462af16ac6751fa6fe9a35132
emma bordres <emma.borders@menloschool.org>
2021-10-12 20:37:18 -0700

    Functions Exercises

---
+++ project0/addCalc.py
@@ -0,0 +1,8 @@
+def adding_numbers(number1, number2):
+    sum = number1 + number2
+    print("the sum of %s" % number1 + " and %s" % number2  + " is %s" % sum)
+
+
+adding_numbers(2, 8)
+adding_numbers(14, -9)
+adding_numbers(-7, 4)
