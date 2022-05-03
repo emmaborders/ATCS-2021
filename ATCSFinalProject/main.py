d9bc5ab615756319e164d23c8be431026f2559cb
emma bordres <emma.borders@menloschool.org>
2022-05-03 14:31:42 -0700

    Outline

---
+++ ATCSFinalProject/main.py
@@ -0,0 +1,25 @@
+import numpy
+import matplotlib.pyplot as plt
+import sklearn
+import matplotlib.image as img
+from PIL import Image
+
+
+def getData(fileName):
+    pass
+
+def pixelColorize(data):
+    pass
+
+def pixelGeometry(data):
+    pass
+
+def monoColorPixels(data):
+    pass
+
+def printImage(data):
+    pass
+
+def main():
+    pass
+
